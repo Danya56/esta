db850143ff7f83c3a629b5a00cdc964254b3080b
Danya56 <danil.goryunov.01@gmail.com>
2026-07-02 12:17:41 +0500

    feat: add category model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
+from typing import Iterable
 from django.db import models
+from pytils.translit import slugify
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
